64c47b5ebd60abd44f15699fd11bd1fc4ce06f51
baramsivaramireddy <baramsivarami@gmail.com>
2024-02-02 17:31:16 +0530

    final

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,17 +21,17 @@ const Header = () =>{
 
     let {userToken, logout} = useAuth() 
 
-    console.log( useAuth() )
+   
     return (
         <>
-            <div className="sticky top-0 z-10  w-full flex flex-row justify-between px-5 py-3 shadow-md items-center">
+            <div className="sticky top-0  z-50 bg-white  w-full flex flex-row justify-between px-5 py-3 shadow-md items-center">
 
                 <div className="text-2xl font-bold "> Algohire blog system</div>
 
                 <div>
 
                     <ul> 
-                      { userToken != null ? <button  className="rounded-full bg-sky-400 py-2 px-5 text-white font-semibold hover:bg-sky-600" onClick={() =>{logout()}}> logout </button>  :  <li className="bg-blue-500 hover:bg-blue-600 py-2 rounded text-white font-bold px-5"> <Link href="/login"> Login</Link> </li>}
+                      { userToken != null ?  <AdminMenu /> :  <li className="bg-blue-500 hover:bg-blue-600 py-2 rounded text-white font-bold px-5"> <Link href="/login"> Login</Link> </li>}
                     </ul>
                 </div>
             </div>
@@ -39,4 +39,18 @@ const Header = () =>{
     )
 }
 
+
+
+const AdminMenu = () =>{
+
+    let {userToken, logout} = useAuth() 
+    return (
+        <>
+            <ul className="flex  items-center gap-5">
+                <li> <Link href={'/add'} className="uppercase hover:bg-green-700 bg-green-500 py-2 px-5 rounded-full text-white "> add </Link> </li>
+                <li><button  className="rounded-full bg-sky-400 py-2 px-5 text-white font-semibold hover:bg-sky-600" onClick={() =>{logout()}}> logout </button></li>
+            </ul>
+        </>
+    )
+}
 export default Layout
